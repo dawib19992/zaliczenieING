a3e62483fe8089e59d4d3ff97ce5fc6b8f384123
dawib19992 <dawidkus1612@gmail.com>
2025-05-31 17:36:46 +0200

    Poprawka

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, World! To moja aplikacja na zaliczenie!');
+    res.send('Zaliczenie Dawid Kuś');
 });
 
 app.listen(port, () => {
